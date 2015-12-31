49e64f8a9042f717b21571c89f69a6f35ee32b73
Shreyan Goswami <overclockedgtx@gmail.com>
2015-12-31 15:51:22 +0530

    First commit

---
+++ botclass.php
@@ -0,0 +1,96 @@
+<?php	
+	class MyTelegramBot{
+		private $token="140012437:AAEVpWTIWvH0PaBNKNRhbXCXmXo_R18Hy2w";
+		private $website="https://api.telegram.org/bot140012437:AAEVpWTIWvH0PaBNKNRhbXCXmXo_R18Hy2w";
+		private $name="@gtxtestbot";
+		private $count=0;
+		private $userlist;
+		private $flags;
+		private $startTxt="*yawns* just woke up";
+		
+		//obtain all the messages recieved by the bot that is stored in the cloud
+		function getUpd(){
+			$update=file_get_contents($this->website."/getupdates");
+			$updateArray=json_decode($update,true);
+			return $updateArray;
+		}
+		
+		//send a message text to chatid
+		function sendMsg($id,$text){
+			file_get_contents($this->website."/sendmessage?chat_id=".$id."&text=".$text);
+		}
+		
+		//replies to a message sent to bot
+		function replyToMsg($id,$msgid,$text){
+			file_get_contents($this->website."/sendmessage?chat_id=".$id."&text=".$text."&reply_to_message_id=".$msgid);
+		}
+		
+		//get the last message recieved by the bot
+		function getLatestMsg(){
+			$updateArray=$this->getUpd();
+			$len=count($updateArray['result']);
+			if($len>0)
+			{
+				$msg=$updateArray["result"][$len-1]["message"];
+				//var_dump($msg);
+				return $msg;
+			}
+		}
+		
+		function updateMsg(){
+			$update=$this->getUpd();
+			$len=count($update['result']);
+			if($len>0){
+				$updid=$update["result"][$len-1]["update_id"]+1;
+				file_get_contents($this->website."/getupdates?offset=".$updid);
+			}
+		}
+		
+		function setBotKey($token){
+			$this->token=$key;
+		}
+		
+		function setName($name){
+			$this->name=$name;
+		}
+		
+		function getStartMsg(){
+			return $this->startTxt;
+		}
+		
+		function getName(){
+			return $this->name;
+		}
+		
+		function fetchMovie($movie){
+			$json=file_get_contents("http://www.omdbapi.com/?t=".$movie."&y=&plot=short&r=json");
+			$info=json_decode($json,true);
+			return $info;
+		}
+		
+		function addUser($id,$title){
+			if($this->findUser($id)==0){
+				$this->userlist[$id]=$title;
+				$this->flags[$id]=1;
+			}
+		}
+		
+		function isStarted($id){
+			return $this->flags[$id];
+		}
+		
+		function removeUser($id){
+			if($this->findUser($id)==1){
+				unset($this->userlist[$id]);
+				$this->flags[$id]=0;
+				$this->userlist=array_values($this->userlist);
+			}
+		}
+		
+		function findUser($id){
+			if(isset($this->userlist[$id])){
+				return 1;
+			}else return 0;
+		}
+	}
+?>
---
+++ imdbbot.php
@@ -0,0 +1,51 @@
+<?php
+	require 'botclass.php';
+	$bot=new MyTelegramBot();
+	
+	while(1){
+		$msg=$bot->getLatestMsg();
+		if(is_null($msg)){
+			continue;
+		}
+		$wrds=explode(' ',$msg['text']);
+		$id=$msg['chat']['id'];
+		$mid=$msg["message_id"];
+		if($wrds[0]=='/rating' || $wrds[0]=='/rating'.$bot->getName()){
+			$mvname="";
+			for($i=1;$i<count($wrds);$i++){
+				if($i!=count($wrds)-1)
+					$mvname.=$wrds[$i]."+";
+				else $mvname.=$wrds[$i];
+			}
+			$info=$bot->fetchMovie($mvname);
+			if($bot->findUser($id)==1){
+				$bot->replyToMsg($id,$mid,$info['imdbRating']);
+			}
+			else{
+				$bot->replyToMsg($id,$mid,"Please start the bot");
+			} 
+			$bot->updateMsg();
+		}else if(($wrds[0]=='/start' || $wrds[0]=='/start'.$bot->getName()) && $bot->isStarted($id)==0){
+			if($bot->findUser($id)==0){
+				$bot->replyToMsg($id,$mid,"Bot started");
+				$type=$msg['chat']['type'];
+				if($type=="private")
+					$title=$msg['chat']['username'];
+				else $title=$msg['chat']['title'];
+				$bot->addUser($id,$title);
+			}else if($bot->findUser($id)==1){
+				$bot->replyToMsg($id,$mid,"Bot already running");
+			}
+				$bot->updateMsg();
+		}else if(($wrds[0]=="/stop" || $wrds[0]=="/stop".$bot->getName()) && $bot->isStarted($id)==1){
+			if($bot->findUser($id)==0){
+				$bot->replyToMsg($id,$mid,"Bot is not running sir");
+			}else if($bot->findUser($id)==1){
+				$bot->replyToMsg($id,$mid,"Bot stopped");
+				$bot->updateMsg();
+				$bot->removeUser($id);
+			}
+		}
+		$msg=NULL;
+	}
+?>
